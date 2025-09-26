756db3aedd667ef76d06efea844759fa9f39fad3
Elyassaa <mbyassaa@gmail.com>
2025-09-26 12:12:58 +0100

    init commit

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String adjective1;
+        String noun1;
+        String adjective2;
+        String verb1;
+        String adjective3;
+
+        System.out.println("Enter an adjective (description): ");
+        System.out.println("Enter a noun (animal Or prerson): ");
+        System.out.println("Enter an adjective (description): ");
+        System.out.println("Enter a verb end with");
+
+
+        System.out.println("Today I went to a "+ adjective1 +" zoo");
+        System.out.println("In an exhibit, I saw a " + noun1 + ".");
+        System.out.println(noun1+ " was "+adjective2+" and "+verb1+ "!");
+        System.out.println("I was "+ adjective3 +"!");
+    }
+
+    }

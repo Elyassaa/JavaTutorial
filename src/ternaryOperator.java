613117fcfcaa79106d09ba9812d675b1c3e8480d
Elyassaa <mbyassaa@gmail.com>
2025-09-27 12:47:09 +0100

    Initial commit ternaryOperator

---
+++ src/ternaryOperator.java
@@ -0,0 +1,8 @@
+public class ternaryOperator {
+    public static void main(String[] args){
+
+        int score = 55;
+        String passOrFail = (score >= 55) ? "Pass" : "Fail";
+        System.out.println(passOrFail);
+    }
+}

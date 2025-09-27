b8c240e18dd94745905ff48d916a09468c4335a3
Elyassaa <mbyassaa@gmail.com>
2025-09-27 12:49:59 +0100

    Initial commit even Odd

--- src/ternaryOperator.java
+++ src/ternaryOperator.java
@@ -3,6 +3,9 @@ public class ternaryOperator {
 
         int score = 55;
         String passOrFail = (score >= 55) ? "Pass" : "Fail";
+        int number = 23;
+        String evenOrOdd = (number % 2 == 0) ? "Even" : "Odd";
         System.out.println(passOrFail);
+        System.out.println(evenOrOdd);
     }
 }

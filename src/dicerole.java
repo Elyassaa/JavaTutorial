ed171c484d892bcce08a5a73b7553f9277dabaf5
Elyassaa <mbyassaa@gmail.com>
2025-10-02 12:22:50 +0100

    Initial commit even Odd

---
+++ src/dicerole.java
@@ -0,0 +1,66 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class dicerole {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        Random random = new Random();
+
+        int inpt;
+        int dice;
+        int j;
+        int count=0;
+        System.out.print("Enter the # of the role you want: ");
+        inpt = scan.nextInt();
+        for (int i=0; i<inpt; i++){
+            dice = random.nextInt(1,7);
+           switch (dice){
+               case 1->{
+                   System.out.println(" ------");
+                   System.out.println("   ⚪  ");
+                   System.out.println(" ------");
+               }
+               case 2->{
+                   System.out.println(" ------");
+                   System.out.println("  ⚪  ");
+                   System.out.println("  ⚪  ");
+                   System.out.println(" ------");
+               }
+               case 3->{
+                   System.out.println(" ------");
+                   System.out.println(" ⚪     ");
+                   System.out.println("   ⚪   ");
+                   System.out.println("     ⚪   ");
+                   System.out.println(" ------");
+               }
+               case 4->{
+                   System.out.println(" ------");
+                   System.out.println(" ⚪  ⚪ ");
+                   System.out.println("        ");
+                   System.out.println(" ⚪  ⚪ ");
+                   System.out.println(" ------");
+               }
+               case 5->{
+                   System.out.println(" ------");
+                   System.out.println(" ⚪  ⚪ ");
+                   System.out.println("   ⚪   ");
+                   System.out.println(" ⚪  ⚪ ");
+                   System.out.println(" ------");
+               }
+               case 6->{
+                   System.out.println(" ------");
+                   System.out.println(" ⚪  ⚪ ");
+                   System.out.println(" ⚪  ⚪ ");
+                   System.out.println(" ⚪  ⚪ ");
+                   System.out.println(" ------");
+               }
+           }
+           System.out.println("Your role: "+ dice);
+           count +=dice;
+        }
+        System.out.println("Total role: "+ count);
+
+
+
+    }
+}
